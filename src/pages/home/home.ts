7667de0329751933f5670b69af0369bca1ca515e
fernandasalas <ArroyoSeco20>
2019-03-25 14:53:50 -0600

    carga de imagenes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  casas = [];
+  constructor(public navCtrl: NavController, 
+    public http: HttpClient) {
 
-  constructor(public navCtrl: NavController) {
+      this.http.get('/v1/klfst?&category=1040&region=16&lang=es&lim=15')
+      .subscribe( data => {
+        console.log(JSON.stringify(data));
+        this.casas = data['list_ads'];
+      }, error => {
+        console.log(JSON.stringify(error));
+      });
 
   }
 
